e528aa668e014d44fb95bf15cce2ae0e3f4efa30
Victor Queiroz <victorcqueirozg@gmail.com>
2018-07-24 18:35:55 -0300

    create an abstract Reporter class

---
+++ src/reporters/reporter.ts
@@ -0,0 +1,73 @@
+import Test from "../test";
+
+export enum ReporterEvents {
+    StartTest,
+    FailTest,
+    SucceedTest,
+    /**
+     * Triggered when runner starts to
+     * read tests of a file
+     */
+    ReadFile,
+    EndTest,
+    /**
+     * Triggered when runner ends reading
+     * of current file
+     */
+    EndFile,
+    Finished
+}
+
+export default abstract class Reporter {
+    public test?: Test;
+    /**
+     * Filename which holds current test
+     */
+    public filename?: string;
+
+    public failuresCount: number = -1;
+    public successesCount: number = -1;
+
+    /**
+     * Current failure description object. It'll be
+     * attached to this reporter as soon as the runner receive
+     * a rejection from a test
+     */
+    public failure?: any;
+
+    constructor(public stdout: NodeJS.WriteStream, public stderr: NodeJS.WriteStream) {}
+    /* tslint:disable member-access */
+    abstract describe(event: ReporterEvents): void;
+    /* tslint:enable member-access */
+    public readFile(filename: string) {
+        this.filename = filename;
+        this.failuresCount = 0;
+        this.successesCount = 0;
+        this.describe(ReporterEvents.ReadFile);
+    }
+    public endFile() {
+        this.describe(ReporterEvents.EndFile);
+        delete this.filename;
+    }
+    public startTest(test: Test) {
+        this.test = test;
+        this.describe(ReporterEvents.StartTest);
+    }
+    public succeedTest() {
+        this.describe(ReporterEvents.SucceedTest);
+        ++this.successesCount;
+    }
+    public failTest(reason: any) {
+        this.failure = reason;
+        this.describe(ReporterEvents.FailTest);
+        delete this.failure;
+        ++this.failuresCount;
+    }
+    public finished() {
+        this.describe(ReporterEvents.Finished);
+    }
+    public endTest() {
+        this.describe(ReporterEvents.EndTest);
+        delete this.test;
+    }
+}
