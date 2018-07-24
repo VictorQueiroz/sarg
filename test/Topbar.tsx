2ff3676a10982b2d68f25d3d218195d2b0b8a6e8
Victor Queiroz <victorcqueirozg@gmail.com>
2018-07-24 18:25:13 -0300

    test/Topbar.tsx: define public as a public method

--- test/Topbar.tsx
+++ test/Topbar.tsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import * as React from 'react';
 
 export default class Topbar extends Component<{}, {}> {
-    render(): JSX.Element {
+    public render(): JSX.Element {
         return (
             <div>
                 Topbar test
