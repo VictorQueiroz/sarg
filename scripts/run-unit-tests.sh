06aa02266e060c806e4f4a0ff9955092b7bb6463
Victor Queiroz <victorcqueirozg@gmail.com>
2019-08-12 13:11:16 -0300

    Makefile: Update test task

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,9 +1,9 @@
-TS_NODE_PROJECT=$PWD/test ts-node src/index.ts \
+TS_NODE_PROJECT=$PWD/test npx ts-node src/index.ts \
     --bail \
-    --require $PWD/test/configure-enzyme.js \
-    --require babel-register \
+    --require $PWD/test/configure-enzyme.ts \
+    --require @babel/register \
     --require source-map-support/register \
-    --ignore ./test/ignore-option/ignore.js \
-    --ignore ./test/failed-comparison.js \
+    --ignore ./test/ignore-option/ignore.ts \
+    --ignore ./test/failed-comparison.ts \
     $@ \
-    "./test/**/*.js"
+    "./test/**/*.*"
